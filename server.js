551d718cb22c7b8f141782e09da178a98ec9f81d
bridget462 <bridges.dev.create@gmail.com>
2020-10-12 20:12:18 +0900

    client redirected to new room

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+const { v4: uuidV4 } = require("uuid");
 
 app.set("view engine", "ejs");
 app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  // create new room and redirect client
+  res.redirect(`/${uuidV4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  // pass roomId to client
+  res.render("room", { roomId: req.params.room });
+});
 
 server.listen(3000);
