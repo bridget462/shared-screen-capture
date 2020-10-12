a709fe82c868fdc2d048d8a81c3420b261f5b414
bridget462 <bridges.dev.create@gmail.com>
2020-10-12 20:36:49 +0900

    screen caputre stream aquried

--- public/script.js
+++ public/script.js
@@ -1,7 +1,22 @@
 const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+navigator.mediaDevices.getDisplayMedia().then((stream) => {
+  addVideoStream(myVideo, stream);
+});
 
 socket.emit("join-room", ROOM_ID, 10);
 
 socket.on("user-connected", (userId) => {
   console.log("new user connected: ", userId);
 });
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
